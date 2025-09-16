fb9574e80fbec816c91b41874ed4ef0a541d988f
IT24104192 <it24104192@my.sliit.lk>
2025-09-16 14:18:12 +0530

    IT24104192_Lab07

---
+++ Lab Sheet 07/IT24104192.R
@@ -0,0 +1,14 @@
+setwd("F:\\SLIIT\\Y2S1\\Probability and Statistics\\Labs\\IT24104192_Lab7")
+
+# 01
+punif(15,min = 0,max = 40, lower.tail = FALSE)
+
+# 02
+pexp(2,rate = 0.333, lower.tail = TRUE)
+
+# 03
+# 1)
+pnorm(130,mean = 100, sd = 15, lower.tail = FALSE)
+
+# 2)
+qnorm(0.95, mean = 100, sd = 15)
